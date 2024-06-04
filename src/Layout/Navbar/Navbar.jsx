5c685854535bbeeadb46204100d51f85583084c7
EjuMir <mirejo101@gmail.com>
2024-06-04 21:13:14 +0600

    finalized navbar

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -40,10 +40,11 @@ const Navbar = () => {
         {/* dashboard will be conditional */}
         <NavLink to='/dashboard' data-tooltip-content={'Dashboard'} data-tooltip-id="dashboard" className={({ isActive }) => isActive ? 'font-bold rounded-lg text-3xl px-4 p-2' : 'text-white font-bold text-[22px] px-4 py-2'} ><Tooltip id="dashboard"></Tooltip><RxDashboard></RxDashboard></NavLink>
 
-        <div className="dropdown">
+        <div data-tooltip-content={'Article'} data-tooltip-id="article" className="dropdown text-[22px] font-bold">
             <div tabIndex={0} role="button" className="btn btn-ghost">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white font-bold" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
+            <Tooltip id="article"></Tooltip>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[2] p-2 shadow bg-red-500 rounded-box w-20">
                 <NavLink to='/addArticle' data-tooltip-content={'Add Article'} data-tooltip-id="addArticle" className={({ isActive }) => isActive ? 'font-bold rounded-lg text-3xl px-4 p-2' : 'text-white font-bold text-[22px] px-4 py-2'} ><Tooltip id="addArticle"></Tooltip><AiFillFileAdd></AiFillFileAdd></NavLink>
                 <NavLink to='/myArticles' data-tooltip-content={'My Articles'} data-tooltip-id="myArticles" className={({ isActive }) => isActive ? 'font-bold rounded-lg text-3xl px-4 p-2' : 'text-white font-bold text-[22px] px-4 py-2'} ><Tooltip id="myArticles"></Tooltip><MdArticle></MdArticle></NavLink>
@@ -91,10 +92,10 @@ const Navbar = () => {
 
             <div className="navbar-end gap-2">
 
-                <NavLink to='/signUp'><a className="btn bg-cyan-600 text-white font-bold">Sign Up</a></NavLink>
+                <NavLink to='/signUp'><a className="btn bg-white-600 text-red-500 font-bold">Sign Up</a></NavLink>
 
                 {
-                    user ? <a onClick={() => logOut()} className="btn bg-red-500 text-white font-bold">Sign Out</a> : <NavLink to='/signIn'><a className="btn bg-cyan-800 text-white font-bold">Sign In</a></NavLink>
+                    user ? <a onClick={() => logOut()} className="btn bg-red-500 text-white font-bold">Sign Out</a> : <NavLink to='/signIn'><a className="btn bg-red-500 text-white font-bold">Sign In</a></NavLink>
 
                 }
 
