c050fa0bc09c669ec81a751892b56531312b801c
EjuMir <mirejo101@gmail.com>
2024-06-06 17:21:56 +0600

    premium user's article are seperated

--- src/Layout/Pages/AllArticles/AllArticles.jsx
+++ src/Layout/Pages/AllArticles/AllArticles.jsx
@@ -6,10 +6,16 @@ const AllArticles = () => {
     const [allNewsArticle] = useAllNews();
     
     return (
+        <div>
+            <div>
+              <h2 className="text-3xl my-10 text-center font-bold">All Articles Are Here</h2>
+            </div>
         <div className="grid grid-cols-1 lg:grid-cols-3 place-items-center gap-10">
+            
             {
                 allNewsArticle.map(article => <ArticleCard key={article._id} article={article}></ArticleCard>)
             }
+        </div>
         </div>
     );
 };
--- src/Layout/Pages/AllArticles/ArticleCard.jsx
+++ src/Layout/Pages/AllArticles/ArticleCard.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FaUser } from "react-icons/fa";
+import { FaStar, FaUser } from "react-icons/fa";
 import { IoEyeSharp } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import useAxiosPublic from "../../../Hooks/useAxiosPublic";
@@ -7,8 +7,8 @@ import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 
 const ArticleCard = ({article}) => {
     
-    const {title, image, publisher, description, views, _id} = article;
-    const setView = parseInt(views)
+    const {title, image, publisher, description, views, _id, subscription} = article;
+    const setView = parseInt(views);
     const [read, setRead] = useState(false);
     const axiosNews = useAxiosPublic(); 
 
@@ -24,34 +24,76 @@ const ArticleCard = ({article}) => {
     }
 
     return (
-        <div className="border-2 border-red-500 my-12 p-4 rounded-xl hover:bg-red-400">
-        <div className="card bg-base-100 shadow-xl image-full">
-            <figure><img src={image} alt="article's picture" /></figure>
-            <div className="card-body">
-                <h2 className="card-title font-bold">{title}</h2>
-                <div className="my-3">{
-                    
-                    read ? <div onClick={handleRead}>
-                         <p>{description}</p>
-                         <a className="text-white font-bold cursor-pointer">See less ...</a>
-                    </div> 
-                    :
-                     <div>
-                        <p>{description.slice(0,50) }</p>
-                        <a onClick={handleRead} className="text-white font-bold cursor-pointer">Read more ...</a>
+        <div>
+        { 
+            subscription == "premium" ? <div className="border-2 border-red-500 my-12 p-4 bg-black rounded-xl hover:bg-red-400">
+            <div className="card bg-base-100 shadow-xl image-full">
+                <figure><img src={image} alt="article's picture" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title font-bold">{title}</h2>
+                    <div className="my-3">{
+                        
+                        read ? <div onClick={handleRead}>
+                             <p>{description}</p>
+                             <a className="text-white font-bold cursor-pointer">See less ...</a>
+                        </div> 
+                        :
+                         <div>
+                            <p>{description.slice(0,50) }</p>
+                            <a onClick={handleRead} className="text-white font-bold cursor-pointer">Read more ...</a>
+                        </div>
+                    }</div>
+    
+                    <div>
+                       <p className="flex font-bold gap-2 border-2 w-fit text-black p-2 rounded-md bg-gray-300 mb-2"><FaUser className="text-xl text-red-600"></FaUser> {publisher}</p>
+                       <p className="flex font-bold gap-2 border-2 w-fit text-black p-2 rounded-md bg-gray-300"><IoEyeSharp className="text-2xl text-red-600"></IoEyeSharp><span>{setView}</span></p>
                     </div>
-                }</div>
-
-                <div>
-                   <p className="flex font-bold gap-2 border-2 w-fit text-black p-2 rounded-md bg-gray-300 mb-2"><FaUser className="text-xl text-red-600"></FaUser> {publisher}</p>
-                   <p className="flex font-bold gap-2 border-2 w-fit text-black p-2 rounded-md bg-gray-300"><IoEyeSharp className="text-2xl text-red-600"></IoEyeSharp><span>{setView}</span></p>
-                </div>
-                <div className="card-actions justify-end mt-10">
-                   <Link to={`/details/${_id}`}><button onClick={handleView} className="btn bg-red-400 text-white font-bold hover:text-red-500">Read Details</button></Link>
+                    
+                    <div className="card-actions justify-between mt-10">
+                    <p className="flex text-lg bg-black text-orange-500 font-bold max-w-fit rounded-lg gap-2 p-2"><FaStar className="text-2xl"></FaStar>Premium</p>
+                       <Link to={`/details/${_id}`}><button onClick={handleView} className="btn bg-red-400 text-white font-bold hover:text-red-500">Read Details</button></Link>
+                       
+                    </div>
+                    
+                    
                 </div>
             </div>
+            </div>
+             
+            :
+            
+            <div className="border-2 border-red-500 my-12 p-4 rounded-xl hover:bg-red-400">
+            <div className="card bg-base-100 shadow-xl image-full">
+                <figure><img src={image} alt="article's picture" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title font-bold">{title}</h2>
+                    <div className="my-3">{
+                        
+                        read ? <div onClick={handleRead}>
+                             <p>{description}</p>
+                             <a className="text-white font-bold cursor-pointer">See less ...</a>
+                        </div> 
+                        :
+                         <div>
+                            <p>{description.slice(0,50) }</p>
+                            <a onClick={handleRead} className="text-white font-bold cursor-pointer">Read more ...</a>
+                        </div>
+                    }</div>
+    
+                    <div>
+                       <p className="flex font-bold gap-2 border-2 w-fit text-black p-2 rounded-md bg-gray-300 mb-2"><FaUser className="text-xl text-red-600"></FaUser> {publisher}</p>
+                       <p className="flex font-bold gap-2 border-2 w-fit text-black p-2 rounded-md bg-gray-300"><IoEyeSharp className="text-2xl text-red-600"></IoEyeSharp><span>{setView}</span></p>
+                    </div>
+                    <div className="card-actions justify-end mt-10">
+                        
+                       <Link to={`/details/${_id}`}><button onClick={handleView} className="btn bg-red-400 text-white font-bold hover:text-red-500">Read Details</button></Link>
+                    </div>
+                </div>
+            </div>
+            </div>
+        }
         </div>
-        </div>
+        
     );
 };
 
