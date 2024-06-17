f8c0db9edbc026a3c3ae5ffd6f2ceed6062d6874
EjuMir <mirejo101@gmail.com>
2024-06-17 22:44:45 +0600

    modal showing successfully

--- src/Layout/DashboardUtils/AllArticles/AllArticlesAdminCard.jsx
+++ src/Layout/DashboardUtils/AllArticles/AllArticlesAdminCard.jsx
@@ -1,12 +1,24 @@
-import { useQuery } from "@tanstack/react-query";
 import useAllUser from "../../../Hooks/useAllUser";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import Swal from "sweetalert2";
+import Modal from 'react-modal';
+import { useState } from "react";
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
 
 const AllArticlesAdminCard = ({ pending, refetch }) => {
 
-    const axiosSecure = useAxiosSecure()
-    const [author] = useAllUser(); 
+    const axiosSecure = useAxiosSecure();
+    const [author] = useAllUser();
     const { title, email, publisher, tags, date, status, _id, description, image } = pending;
 
     const findAuthor = author.filter(e => e.email == email);
@@ -14,30 +26,32 @@ const AllArticlesAdminCard = ({ pending, refetch }) => {
     const authorName = findAuthor.map(e => e.name);
 
     //Approve Post
-    const handleApprove = async(pending, _id) =>{
-         const statusChange = await axiosSecure.patch(`/articleReq/${_id}`);
-         console.log(statusChange.data.matchedCount);
-         if(statusChange.data.matchedCount){
-             refetch();
-             Swal.fire({
-                 position: "top-end",
-                 icon: "success",
-                 title: "Article Approved",
-                 showConfirmButton: false,
-                 timer: 1500,
-             });
-         }
-         
-         const postInfo = {
+    const handleApprove = async (pending, _id) => {
+        const statusChange = await axiosSecure.patch(`/articleReq/${_id}`,{
+            status: 'Approved'
+        });
+        console.log(statusChange.data.matchedCount);
+        if (statusChange.data.matchedCount) {
+            refetch();
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Article Approved",
+                showConfirmButton: false,
+                timer: 1500,
+            });
+        }
+
+        const postInfo = {
             id: pending._id,
             title: pending.title,
             image: pending.image,
             publisher: pending.publisher,
             tags: pending.tags,
             description: pending.description,
-            views : 0,
-            subscription : "normal",
-         }
+            views: 0,
+            subscription: "normal",
+        }
 
         const articlePost = await axiosSecure.post('/allNews', postInfo)
         console.log(articlePost);
@@ -45,43 +59,87 @@ const AllArticlesAdminCard = ({ pending, refetch }) => {
     }
 
     //Delete Post
-    const handleDelete = async(_id) => {
-        const deleteArticleList = await axiosSecure.delete(`/articleReq/${_id}`);
-        if(deleteArticleList.data.deletedCount > 0){
+    const handleDelete = async (id) => {
+        const deleteOne = await axiosSecure.delete(`/allNews/${id}`);
+        console.log(deleteOne);
+        const deleteArticleList = await axiosSecure.delete(`/articleReq/${id}`);
+        if (deleteArticleList.data.deletedCount > 0) {
             refetch();
             Swal.fire({
-                icon:'success',
-                title:'Article Deleted Successfully',
+                icon: 'success',
+                title: 'Article Deleted Successfully',
                 showConfirmButton: false,
                 timer: 1500
             });
         }
     }
-    
+
+    //Decline Post
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    const handleDecline = (e) => { 
+        e.preventDefault();
+        const form = e.target.reason.value;
+        const decline = axiosSecure.patch(`/articleReq/${_id}`, {
+            declineReason : form,
+            status : "Declined"
+        })
+        console.log(decline);
+    }
 
     return (
         <div className="card w-80 bg-base-100 shadow-xl">
             <div className="card-body">
                 <h2 className="card-title text-lg font-bold">{title}</h2>
                 <div className="flex gap-8">
-                <div><img className="h-14 w-14 rounded-full" src={authorImage} alt="author picture" /></div>
-                <div>
-                <div><h2 className="font-bold">{authorName}</h2></div>
-                <div><h2>{email}</h2></div>
-                </div>
+                    <div><img className="h-14 w-14 rounded-full" src={authorImage} alt="author picture" /></div>
+                    <div>
+                        <div><h2 className="font-bold">{authorName}</h2></div>
+                        <div><h2>{email}</h2></div>
+                    </div>
                 </div>
                 <div><h2>{date}</h2></div>
                 <div><h2>Publisher : <span className="italic">{publisher}</span></h2></div>
                 <div className="mb-2"><h2>{status}</h2></div>
                 {
-                  status == "Pending" && <div className="card-actions justify-center">
-                  <button onClick={()=>handleApprove(pending, _id)} className="btn bg-green-400 text-cyan-700 font-bold">Approve</button>
-                  <button className="btn bg-gray-300 text-red-600 font-bold">Decline</button>
-                  <button onClick={()=>handleDelete(_id)} className="btn bg-red-600 text-white font-bold">Delete</button>
-                  <button className="btn bg-black text-orange-500 font-bold">Make Premium</button>
-              </div> 
+                    status == "Pending" ? <div className="card-actions justify-center">
+                        <button onClick={() => handleApprove(pending, _id)} className="btn bg-green-400 text-cyan-700 font-bold">Approve</button>
+                        <div>
+                            <button className="btn bg-gray-300 text-red-600 font-bold" onClick={openModal}>Decline</button>
+                            <Modal
+                                isOpen={modalIsOpen}
+                                onRequestClose={closeModal}
+                                style={customStyles}
+                                contentLabel="Example Modal"
+                            >
+                                <h2 className="mb-2">Reason</h2>
+                                <form onSubmit={handleDecline}>
+                                    <div>
+                                        <textarea className="border border-black" name="reason" id="reason"></textarea>
+                                    </div>
+                                    <div className="mt-2"><input className="btn bg-green-700 text-white" type="submit" value="Submit" /></div>
+                                </form>
+                                <button className="btn btn-error mt-2 mr-2 font-bold text-xl text-white hover:bg-gray-400" onClick={closeModal}>X</button>
+
+                            </Modal>
+                        </div>
+                        <button onClick={() => handleDelete(_id)} className="btn bg-red-600 text-white font-bold">Delete</button>
+                        <button className="btn bg-black text-orange-500 font-bold">Make Premium</button>
+                    </div> :
+                        <div>
+                            <button onClick={() => handleDelete(_id)} className="btn bg-red-600 text-white font-bold mr-2">Delete</button>
+                            <button className="btn bg-black text-orange-500 font-bold">Make Premium</button>
+                        </div>
                 }
-                
+
             </div>
         </div>
     );
--- src/Layout/Pages/MyArticle/MyArticle.jsx
+++ src/Layout/Pages/MyArticle/MyArticle.jsx
@@ -1,72 +1,119 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthFirebase } from "../../../Authentication/Firebase";
 import useArticleReq from "../../../Hooks/useArticleReq"
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import Modal from 'react-modal';
+
+//styles for modal
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        backgroundColor : 'pink'
+    },
+};
+
 
 const MyArticle = () => {
-    const {user} = useContext(AuthFirebase);
+    const { user } = useContext(AuthFirebase);
     const [article, refetch] = useArticleReq();
     const axiosSecure = useAxiosSecure();
 
 
     const filter = article.filter(element => element.email === user?.email);
 
-    const handleDelete = async(id) => {
+    const handleDelete = async (id) => {
 
-       const deleteOne = await axiosSecure.delete(`/allNews/${id}`);
-          console.log(deleteOne);
+        const deleteOne = await axiosSecure.delete(`/allNews/${id}`);
+        console.log(deleteOne);
 
-         const deleteArticleList = await axiosSecure.delete(`/articleReq/${id}`);
-         console.log(deleteArticleList);
-         if(deleteArticleList.data.deletedCount > 0){
+        const deleteArticleList = await axiosSecure.delete(`/articleReq/${id}`);
+        console.log(deleteArticleList);
+        if (deleteArticleList.data.deletedCount > 0) {
             refetch();
             Swal.fire({
-                icon:'success',
-                title:'Article Deleted Successfully',
-                showConfirmButton:false,
-                timer:1500
+                icon: 'success',
+                title: 'Article Deleted Successfully',
+                showConfirmButton: false,
+                timer: 1500
             })
-         }
+        }
     }
+
+    //modal showing reason
+
+    const [showModal, setShowModal] = useState(undefined);
+    const handleClose = () => setShowModal(undefined);
+    const handleShow = (id) => setShowModal(id);
+
 
     return (
         <div>
-        <div className="flex justify-evenly my-4">
-        </div>
-        <div className="overflow-x-auto">
-            <table className="table table-zebra w-full">
-                {/* head */}
-                <thead>
-                    <tr>
-                        <th>Serial No.</th>
-                        <th>Article Title</th>
-                        <th>Status</th>
-                        <th>isPremium</th>
-                        <th>Action</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        filter.map((article, index) => <tr key={article._id}>
-                            <th>{index + 1}</th>
-                            <td>{article.title}</td>
-                            <td>{article.status}</td>
-                            <td>{article.subscription == "premium" ? 'Yes' : 'No'}</td>
-                            <td>
-                               <Link to={`/details/${article._id}`}><button className="btn btn-accent  mr-2">Details</button></Link>
-                                <button className="btn btn-primary mr-2">Update</button>
-                                <button onClick={()=>handleDelete(article._id)} className="btn bg-red-600 text-white">Delete</button>
-                            </td>
-                        </tr>)
-                    }
+            <div onClick={handleClose} className="flex justify-evenly my-4">
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Serial No.</th>
+                            <th>Article Title</th>
+                            <th>Status</th>
+                            <th>isPremium</th>
+                            <th>Action</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            filter.map((article, index) => <tr key={article._id}>
+                                <th>{index + 1}</th>
+                                <td>{article.title}</td>
+                                <td>{article.status == "Declined" ?
+                                    <div className="flex gap-1 justify-items-center">
+                                        <div>{article.status}</div>
+                                        <div onClick={() => handleShow(article._id)} className="p-1 bg-white border-2 rounded-md text-blue-600 hover:bg-blue-600 hover:text-white hover:cursor-pointer"><h2>Reason</h2></div>
+                                        <div>
+                                            <Modal
+                                                style={customStyles}
+                                                isOpen={showModal == article._id}
+                                                onRequestClose={handleClose}
+                                                contentLabel="Example Modal"
+                                                ariaHideApp={false}
+                                            >
+                                                <div className="text-center">
+                                                    <h2 className="mb-2 text-red-700 font-bold">Reason For Declining : </h2>
+                                                    <div className="border-2 border-red-600 p-2 rounded-md">
+                                                        <h2>{article.declineReason}</h2>
+                                                    </div>
+                                                    <button className="btn btn-error mt-2 mr-2 font-bold text-xl text-white hover:bg-gray-400" onClick={handleClose}>X</button>
+                                                </div>
+                                            </Modal>
+                                        </div>
+                                    </div>
+                                    : <div>{article.status}</div>}
+                                </td>
+                                <td>{article.subscription == "premium" ? 'Yes' : 'No'}</td>
+                                <td>
+                                    <Link to={`/details/${article._id}`}><button className="btn btn-accent text-white mr-2">Details</button></Link>
+                                    {
+                                       article.status == "Declined" ?  '' : <button className="btn btn-primary mr-2">Update</button>
+                                    }
+                                    <button onClick={() => handleDelete(article._id)} className="btn bg-red-600 text-white">Delete</button>
+                                </td>
+                            </tr>)
+                        }
 
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
     );
 };
 
